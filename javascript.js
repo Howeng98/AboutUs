cefe84c6681c3c6f5d5b0803d43ba70e8a1e3edd
howeng98 <leonhwlee9@gmail.com>
2020-04-09 05:21:14 +0800

    Final Version

--- javascript.js
+++ javascript.js
@@ -1,18 +1,97 @@
-$('.banner').click(function(){
-    $('html,body').animate({scrollTop:$('#sec01').offset().top},1300);
-    $('#pic_01').toggleClass('profilePic_fading');
+$('.chien').click(function(){
+    $('html,body').animate({scrollTop:$('#member_06').offset().top - 300},3000);
+    $('#member_01').addClass('member_01_fading delay_04s');
+    $('#id_01').addClass('member_01_id_fading delay_04s');
+    $('#desc_01').addClass('member_01_desc_fading delay_04s'); 
+    $('#member_02').addClass('member_02_fading delay_05s');
+    $('#id_02').addClass('member_02_id_fading delay_05s');
+    $('#desc_02').addClass('member_02_desc_fading delay_05s');
+    $('#member_03').addClass('member_03_fading delay_06s');
+    $('#id_03').addClass('member_03_id_fading delay_06s');
+    $('#desc_03').addClass('member_03_desc_fading delay_06s');
+    $('#member_04').addClass('member_04_fading delay_07s');
+    $('#id_04').addClass('member_04_id_fading delay_07s');
+    $('#desc_04').addClass('member_04_desc_fading delay_07s'); 
+    $('#member_05').addClass('member_05_fading delay_08s');
+    $('#id_05').addClass('member_05_id_fading delay_08s');
+    $('#desc_05').addClass('member_05_desc_fading delay_08s');
+    $('#member_06').addClass('member_06_fading');
+    $('#id_06').addClass('member_06_id_fading');
+    $('#desc_06').addClass('member_06_desc_fading');
 });
 
-$('#sec01').click(function(){
-    $('html,body').animate({scrollTop:$('.banner_text').offset().top},1300);
+$('.howeng').click(function(){
+    $('html,body').animate({scrollTop:$('#member_05').offset().top - 300},2800);
+    $('#member_01').addClass('member_01_fading delay_04s');
+    $('#id_01').addClass('member_01_id_fading delay_04s');
+    $('#desc_01').addClass('member_01_desc_fading delay_04s'); 
+    $('#member_02').addClass('member_02_fading delay_05s');
+    $('#id_02').addClass('member_02_id_fading delay_05s');
+    $('#desc_02').addClass('member_02_desc_fading delay_05s');
+    $('#member_03').addClass('member_03_fading delay_06s');
+    $('#id_03').addClass('member_03_id_fading delay_06s');
+    $('#desc_03').addClass('member_03_desc_fading delay_06s');
+    $('#member_04').addClass('member_04_fading delay_07s');
+    $('#id_04').addClass('member_04_id_fading delay_07s');
+    $('#desc_04').addClass('member_04_desc_fading delay_07s'); 
+    $('#member_05').addClass('member_05_fading');
+    $('#id_05').addClass('member_05_id_fading');
+    $('#desc_05').addClass('member_05_desc_fading');
 });
 
-$('#sec02').click(function(){
-    $('html,body').animate({scrollTop:$('.banner_text').offset().top},1300);
+$('.tzuyu').click(function(){
+    $('html,body').animate({scrollTop:$('#member_03').offset().top - 300},1500);
+    $('#member_01').addClass('member_01_fading delay_04s');
+    $('#id_01').addClass('member_01_id_fading delay_04s');
+    $('#desc_01').addClass('member_01_desc_fading delay_04s'); 
+    $('#member_02').addClass('member_02_fading delay_05s');
+    $('#id_02').addClass('member_02_id_fading delay_05s');
+    $('#desc_02').addClass('member_02_desc_fading delay_05s');
+    $('#member_03').addClass('member_03_fading');
+    $('#id_03').addClass('member_03_id_fading');
+    $('#desc_03').addClass('member_03_desc_fading');
+     
+})
+
+$('.sandy').click(function(){
+    $('html,body').animate({scrollTop:$('#member_02').offset().top - 300},2000);4
+    $('#member_01').addClass('member_01_fading delay_04s');
+    $('#id_01').addClass('member_01_id_fading delay_04s');
+    $('#desc_01').addClass('member_01_desc_fading delay_04s'); 
+    $('#member_02').addClass('member_02_fading');
+    $('#id_02').addClass('member_02_id_fading');
+    $('#desc_02').addClass('member_02_desc_fading');  
+    
 });
 
+$('.yuen').click(function(){
+    $('html,body').animate({scrollTop:$('#member_01').offset().top - 300},2000);
+    $('#member_01').addClass('member_01_fading');
+    $('#id_01').addClass('member_01_id_fading');
+    $('#desc_01').addClass('member_01_desc_fading');        
+});
 
-$('#sec03').click(function(){
-    $('html,body').animate({scrollTop:$('.banner_text').offset().top},1300);
+$('.boo').click(function(){
+    $('html,body').animate({scrollTop:$('#member_04').offset().top - 300},2000);
+    $('#member_01').addClass('member_01_fading delay_04s');
+    $('#id_01').addClass('member_01_id_fading delay_04s');
+    $('#desc_01').addClass('member_01_desc_fading delay_04s'); 
+    $('#member_02').addClass('member_02_fading delay_05s');
+    $('#id_02').addClass('member_02_id_fading delay_05s');
+    $('#desc_02').addClass('member_02_desc_fading delay_05s');
+    $('#member_03').addClass('member_03_fading delay_06s');
+    $('#id_03').addClass('member_03_id_fading delay_06s');
+    $('#desc_03').addClass('member_03_desc_fading delay_06s');
+    $('#member_04').addClass('member_04_fading');
+    $('#id_04').addClass('member_04_id_fading');
+    $('#desc_04').addClass('member_04_desc_fading'); 
+});
+
+$('.top').click(function(){
+    $('html,body').animate({scrollTop:$('.banner').offset().top},1800);
+});
+
+$('.top_context').click(function(){
+    $('html,body').animate({scrollTop:$('.banner').offset().top},1800);
 });
 
